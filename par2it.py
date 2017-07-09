495a5392b9da5b7fd45d3becfa74fe777f6f5340
brent johnson <brentj433@gmail.com>
2017-07-08 18:24:08 -0700

    simplifies code; removes specifying individual files on the command line, which removes the need for the queue

--- par2it.py
+++ par2it.py
@@ -4,44 +4,25 @@ import os
 import sys
 import signal
 import subprocess
-from myqueue import Queue
+
 
 # 2017 Brent Johnson
 # https://github.com/energyvault462/par2it
 
 class ParWrapper:
-    def __init__(self):
-        self.q = Queue()
 
     def TraverseFolder(self, baseFolder = "."):
         for root, dirs, files in os.walk(baseFolder):
-            path = root.split(os.sep)
-            for file in files:
-                filestr = root
-                filestr += "/"
-                filestr += file
-                self.q.enqueue(filestr)
-            #self.q.printqueue()
-            #print "- " *10
-            #self.q.printqueue()
             self.CallPar(root)
-            #print "-" * 10
-            #self.q.printqueue()
 
-    def CallPar(self, folder):
-        targetfiles = ""
-        folder.replace(" ", "\ ")
-        if self.q.size() > 0:
-            while (self.q.size() > 0):
-                #print self.q.dequeue()
-                targetfiles += "\"" + self.q.dequeue() + "\" "
-            targetfiles.replace(" ", "-")
-            parcommand = "par2 create "
-            parcommand += "\"" + folder + "/par2it.par\" " + targetfiles
-            print "-" * 10
-            print "callpar: %s" % parcommand
-            subprocess.call(parcommand, shell=True)
-            print "-" * 10
+
+    def CallPar(self, path):
+        parcommand = "par2 create "
+        parcommand += "\"" + path + "/.par2it\" \"" + path + "/*\""
+        print "-" * 10
+        print "callpar: %s" % parcommand
+        subprocess.call(parcommand, shell=True)
+        print "-" * 10
 
 def main(argv):
     p = ParWrapper()
