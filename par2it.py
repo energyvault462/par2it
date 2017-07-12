262ebfde8bf24e01ed32d61e236bc537d8f7be31
brent johnson <brentj433@gmail.com>
2017-07-11 21:16:42 -0700

    working on fixing par2 c being run more than once if par files already exist

--- par2it.py
+++ par2it.py
@@ -10,19 +10,55 @@ import subprocess
 # https://github.com/energyvault462/par2it
 
 class ParWrapper:
-
     def TraverseFolder(self, baseFolder = "."):
         for root, dirs, files in os.walk(baseFolder):
-            self.CallPar(root)
+            if self.VerifyPars(root) > 0:
+                print "***> Run CreatePar: %s" % root
+                self.CreatePars(root)
+            else:
+                print "***> Pars already exist for %s" % root
 
+    def GetParList(self, path):
+        import glob
+        searchstr = path + "/.par2it*"
+        list = glob.glob(searchstr)
+        return list
 
-    def CallPar(self, path):
-        parcommand = "par2 create "
+    def VerifyPars(self, path):
+        filesToCheck = ""
+        exitcode = 0
+        parfiles = []
+        parfiles = self.GetParList(path)
+        if len(parfiles) == 9:
+            for parfile in parfiles:
+                filesToCheck += "\"" + parfile + "\" "
+            #print "files to check: %s" % filesToCheck
+            parcommand = "par2 v "
+            parcommand += filesToCheck
+            rc = subprocess.call(parcommand, shell=True)
+            if rc > 0:
+                exitcode = rc
+        else:
+            exitcode = 999
+
+        return exitcode
+
+    def DeleteParFiles(self, path):
+        parfiles = self.GetParList(path)
+        for parfile in parfiles:
+            fileToDelete = ""
+            fileToDelete += path + "/" + parfile
+            try:
+                print("Delete: %s") % fileToDelete
+                os.remove(fileToDelete.encode('string-escape'))
+            except OSError:
+                pass
+
+    def CreatePars(self, path):
+        self.DeleteParFiles(path)
+        parcommand = "par2 create -n8 "
         parcommand += "\"" + path + "/.par2it\" \"" + path + "/*\""
-        print "-" * 10
-        print "callpar: %s" % parcommand
         subprocess.call(parcommand, shell=True)
-        print "-" * 10
 
 def main(argv):
     p = ParWrapper()
