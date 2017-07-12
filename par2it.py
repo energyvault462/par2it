52a08a96392d791a9f67e9d26a5b9f453f242427
brent johnson <brentj433@gmail.com>
2017-07-12 15:52:14 -0700

    adds a unit test

--- par2it.py
+++ par2it.py
@@ -1,60 +1,11 @@
 
 #!/usr/bin/python
-import os
 import sys
-import signal
-import subprocess
-
+from parwrapper import ParWrapper
 
 # 2017 Brent Johnson
 # https://github.com/energyvault462/par2it
 # TODO: Travis CI tests
-
-class ParWrapper:
-    def TraverseFolder(self, baseFolder = "."):
-        for root, dirs, files in os.walk(baseFolder):
-            if self.VerifyPars(root) > 0:
-                self.CreatePars(root)
-            else:
-                print "***> Pars already exist for %s" % root
-
-    def GetParList(self, path):
-        import glob
-        searchstr = path + "/.par2it*"
-        list = glob.glob(searchstr)
-        return list
-
-    def VerifyPars(self, path):
-        filesToCheck = ""
-        exitcode = 0
-        parfiles = []
-        parfiles = self.GetParList(path)
-        if len(parfiles) == 9:
-            for parfile in parfiles:
-                filesToCheck += "\"" + parfile + "\" "
-            parcommand = "par2 v "
-            parcommand += filesToCheck
-            rc = subprocess.call(parcommand, shell=True)
-            if rc > 0:
-                exitcode = rc
-        else:
-            exitcode = 999
-        return exitcode
-
-    def DeleteParFiles(self, path):
-        parfiles = self.GetParList(path)
-        for parfile in parfiles:
-            try:
-                print("Delete: %s") % parfile
-                os.remove(parfile.encode('string-escape'))
-            except OSError:
-                pass
-
-    def CreatePars(self, path):
-        self.DeleteParFiles(path)
-        parcommand = "par2 create -n8 "
-        parcommand += "\"" + path + "/.par2it\" \"" + path + "/*\""
-        subprocess.call(parcommand, shell=True)
 
 def main(argv):
     p = ParWrapper()
---
+++ tests/test_par2it.py
@@ -0,0 +1,20 @@
+import pytest
+import os, io
+from parwrapper import ParWrapper
+
+def createtestfiles():
+    filelist = [".par2it.par2", ".par2it.vol000+01.par2", ".par2it.vol001+02.par2", ".par2it.vol003+04.par2", ".par2it.vol007+08.par2"]
+    for file in filelist:
+        with io.open("./tests/" + file, 'w', encoding='utf-8') as f:
+            f.write(u'test')
+
+@pytest.fixture()
+def parwrapper():
+    createtestfiles()
+    pw = ParWrapper()
+    yield pw
+    pw.DeleteParFiles('./tests')
+
+def test_getparlist(parwrapper):
+    parlist = parwrapper.GetParList('./tests')
+    assert len(parlist) == 5
