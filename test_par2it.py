57ca8bb97f301325c3d31de638b6b417c717d6b4
brent johnson <brentj433@gmail.com>
2017-07-12 17:17:41 -0700

    working on travis

--- test_par2it.py
+++ test_par2it.py
@@ -6,7 +6,7 @@ from parwrapper import ParWrapper
 def createtestfiles():
     filelist = [".par2it.par2", ".par2it.vol000+01.par2", ".par2it.vol001+02.par2", ".par2it.vol003+04.par2", ".par2it.vol007+08.par2"]
     for file in filelist:
-        with io.open("./tests/" + file, 'w', encoding='utf-8') as f:
+        with io.open(file, 'w', encoding='utf-8') as f:
             f.write(u'test')
 
 @pytest.yield_fixture()  # yield_fixture instead of just .fixture due to travis ci
@@ -14,8 +14,8 @@ def parwrapper():
     createtestfiles()
     pw = ParWrapper()
     yield pw
-    pw.DeleteParFiles('./tests')
+    pw.DeleteParFiles('.')
 
 def test_getparlist(parwrapper):
-    parlist = parwrapper.GetParList('./tests')
+    parlist = parwrapper.GetParList('.')
     assert len(parlist) == 5
